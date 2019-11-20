4ff35f4c0e900b0954d9f369263777c32dc77a91
Alejandro Hoyos <0670alejo@gmail.com>
2019-11-20 16:38:34 -0500

    initial

---
+++ bin/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+const eventController = require('../controllers/eventController.js');
+
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
+
+//Middleware to recieve big files and prevent problems
+app.use(bodyParser.json({
+    limit: "50mb"
+}));
+app.use(bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
+
+app.use(express.static(__dirname + '/static', {
+    dotfiles: 'allow'
+}))
+
+app.listen(3001, () => {
+    console.log('Started on port 3001');
+});
+
+app.post('/saveEvent', function (req, res) {
+    let result = eventController.create(req.body);
+
+    res.send(JSON.stringify(result, null, 2));
+})
---
+++ controllers/eventController.js
@@ -0,0 +1,49 @@
+const Db = require('mongodb').Db;
+const MongoClient = require('mongodb').MongoClient;
+const propertiesReader = require('properties-reader');
+const ObjectID = require('mongodb').ObjectID;
+const assert = require('assert');
+
+var properties = propertiesReader('./properties/constants.properties');
+const dataBaseRoute = properties.get('main.mongo.dataBaseRoute');
+const ingenieriaCollection = properties.get('main.mongo.Ingenieria');
+
+const eventos = properties.get('main.mongo.eventos');
+
+module.exports.create = (jsonRecord) => {
+    return createEvent(jsonRecord);
+};
+
+async function createEvent(deviceRecord) {
+    console.log(deviceRecord.name)
+    MongoClient.connect(dataBaseRoute, {
+        native_parser: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+        function (err, client) {
+            if (err) throw err;
+            var db = client.db(ingenieriaCollection);
+            db.collection('eventos').insertOne({
+                name: deviceRecord.name,
+                place: deviceRecord.place
+            },
+                (err, resultado) => {
+                    let result = {};
+                    if (err) {
+                        result.response = 0;
+                        result.message = `Error al crear el usuario ${err}`
+                        //In case of an error
+                        return (result);
+                    } else {
+                        result.response = 1;
+                        result.message = `Record creado exitosamente`
+                        //Return the new result
+                        return (result);
+                    }
+
+                });
+            client.close();
+        });
+
+}
